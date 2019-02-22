9ddb7694526825c109807ae072c9fcdc4efab39d
codyKiedaisch <kiedaisch.cody@gmail.com>
2019-02-22 01:10:44 +0000

    assign1

--- assign1/my-unzip.c
+++ assign1/my-unzip.c
@@ -7,15 +7,14 @@
 
 #define EOL 999999
 
+int x =0;
+
 int main (int argc, char *argv[])
 {
-size_t size = 32
-int count= 0;
-
 FILE *fp = fopen("file.txt", "r");
 	if(fp == NULL)
 	{
-	printf("my-zip: cannot open file\n");
+	printf("my-unzip: cannot open file\n");
 	exit(1);
 	}
 	else
@@ -23,12 +22,15 @@ FILE *fp = fopen("file.txt", "r");
 		char buffer[EOL];
 		while(getline(&buffer, &size, stdin))
 		{
-		for(i=0;argv[i]=argv[i+1];i++)
+		for(i=1;argv[i];i++)
 		{
-		count++;
+		i++;
+		for(j=0;j<=i;j++)
+		{
+		printf(argv[x], stdout);
 		}
-		fwrite(const void, *argv[0]+count, size, stdout, FILE *file.txt)
-		printf("%s",stdout);
+		x+2;
+		}
 		}
 	}
 fclose(fp);
